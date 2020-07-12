f6676e060b9521b421ace15bea913a7a90342e6e
Juyoung <juyoung2677@gmail.com>
2020-07-13 01:54:13 +0900

    recycler view added

--- app/src/main/java/com/example/chatapp/ChatActivity.java
+++ app/src/main/java/com/example/chatapp/ChatActivity.java
@@ -1,12 +1,17 @@
 package com.example.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class ChatActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    MyAdapter mAdapter; // 생성한 어댑터로 지정
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,21 @@ public class ChatActivity extends AppCompatActivity {
                 finish(); // 앱 종료 명령어
             }
         });
+
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        String[] myDataset = {"test1", "test2", "test3", "test4"};
+        mAdapter = new MyAdapter(myDataset); // 어댑터 클래스도 구현 필요
+        recyclerView.setAdapter(mAdapter);
 
 
 
