dbc4dae29a96ab235cb376297587ccf202f8b826
Juyoung <juyoung2677@gmail.com>
2020-07-13 15:34:44 +0900

    database read to console

--- app/src/main/java/com/example/chatapp/ChatActivity.java
+++ app/src/main/java/com/example/chatapp/ChatActivity.java
@@ -5,30 +5,39 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Comment;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Hashtable;
 
 public class ChatActivity extends AppCompatActivity {
+    private static final String TAG = "ChatActivity";
     private RecyclerView recyclerView;
     MyAdapter mAdapter; // 생성한 어댑터로 지정
     private RecyclerView.LayoutManager layoutManager;
     EditText etText;
     Button btnSend;
     String stEmail;
+    FirebaseDatabase database; // 전역변수로 선언
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        database = FirebaseDatabase.getInstance();
 
         stEmail = getIntent().getStringExtra("email"); // 메인으로부터 이메일 받아오는 코드
 
@@ -57,12 +66,71 @@ public class ChatActivity extends AppCompatActivity {
         mAdapter = new MyAdapter(myDataset); // 어댑터 클래스도 구현 필요
         recyclerView.setAdapter(mAdapter);
 
+        // 하위 데이터 받아오는 코드 from 파이어베이스 문서
+        ChildEventListener childEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
+                Log.d(TAG, "onChildAdded:" + dataSnapshot.getKey());
+
+                // A new comment has been added, add it to the displayed list
+                Chat chat = dataSnapshot.getValue(Chat.class);
+                String commentKey = dataSnapshot.getKey();
+                String stEmail = chat.getEmail();
+                String stText = chat.getText();
+                Log.d(TAG, "stEmail: "+stEmail);
+                Log.d(TAG, "stText: "+stText);
+
+                // ...
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String previousChildName) {
+                Log.d(TAG, "onChildChanged:" + dataSnapshot.getKey());
+
+                // A comment has changed, use the key to determine if we are displaying this
+                // comment and if so displayed the changed comment.
+
+
+                // ...
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                Log.d(TAG, "onChildRemoved:" + dataSnapshot.getKey());
+
+                // A comment has changed, use the key to determine if we are displaying this
+                // comment and if so remove it.
+                String commentKey = dataSnapshot.getKey();
+
+                // ...
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String previousChildName) {
+                Log.d(TAG, "onChildMoved:" + dataSnapshot.getKey());
+
+                // A comment has changed position, use the key to determine if we are
+                // displaying this comment and if so move it.
+
+                // ...
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "postComments:onCancelled", databaseError.toException());
+                Toast.makeText(ChatActivity.this, "Failed to load comments.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        };
+        DatabaseReference ref = database.getReference("message");
+        ref.addChildEventListener(childEventListener);
+        
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String stText = etText.getText().toString();
                 Toast.makeText(ChatActivity.this, "MSG : "+stText, Toast.LENGTH_LONG).show();
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
+
 
                 Calendar c = Calendar.getInstance();
                 SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
